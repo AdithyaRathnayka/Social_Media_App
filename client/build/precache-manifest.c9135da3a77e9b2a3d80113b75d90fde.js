7ef33b52c0504ea7997b13d2880239bdb3e1ba31
AdithyaRathnayaka <adikawmini21@gmail.com>
2023-06-21 23:25:55 +0530

    build folder added

---
+++ client/build/precache-manifest.c9135da3a77e9b2a3d80113b75d90fde.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8fcdbbd17794805bb82d",
+    "url": "/static/css/main.d8ae5b65.chunk.css"
+  },
+  {
+    "revision": "8fcdbbd17794805bb82d",
+    "url": "/static/js/main.8fcdbbd1.chunk.js"
+  },
+  {
+    "revision": "fbabc16467fb23effdd5",
+    "url": "/static/js/1.fbabc164.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7ce8314d7b4f3de41f2458439359d971",
+    "url": "/index.html"
+  }
+];
